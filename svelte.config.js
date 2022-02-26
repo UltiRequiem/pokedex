0a6fe249dcceb666d96d1704fb0a6d797877a787
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-26 16:56:53 -0500

    :wrench: Update Svelte Config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
+  adapter: adapter(),
 };
 
 export default config;
